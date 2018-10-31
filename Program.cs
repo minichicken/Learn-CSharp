0fa85160feaab761b8a516dceedcd780ccbc99be
minichicken <mini.chicken.dev@gmail.com>
2018-10-31 23:17:06 +0900

    initialize commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Learn_CSharp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
